3e60fbe1dcb2798de569d00725545eaa300d7847
Andrew Tetteh <tettehandrew18@gmail.com>
2023-06-16 22:11:03 +0000

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * description: prints all possible combinations of two-digit numbers
+ *
+ * Return: 0 always
+ */
+
+int main(void)
+{
+	int a, b, c, d;
+	int count = 0;
+
+	for (a = 48; a <= 54; a++)
+	{
+		for (b = a + 1; b <= 55; b++)
+		{
+			for (c = b + 1; b <= 56; c++)
+			{
+				for (d = c + 1; d <= 57; d++)
+				{
+					putchar(a);
+					putchar(b);
+					putchar(32);
+					putchar(c);
+					putchar(d);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
