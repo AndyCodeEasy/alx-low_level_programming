c9d7c048ea38349bcf8814c77aa09b748416ee7d
Andrew Tetteh <tettehandrew18@gmail.com>
2023-06-21 20:09:14 +0000

    Even Liber Abbaci

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -9,18 +9,18 @@
 
 int main(void)
 {
-	long int fib[34];
+	long int fib[33];
 	int a;
 	long int sum = 0;
 
 	fib[0] = 1;
 	fib[1] = 2;
 
-	for (a = 2; a < 35; a++)
+	for (a = 2; a < 33; a++)
 	{
 		fib[a] = fib[a - 1] + fib[a - 2];
 	}
-	for (a = 0; a < 35; a++)
+	for (a = 0; a < 33; a++)
 	{
 		if (fib[a] % 2 == 0)
 			sum += fib[a];
