e143955c30d0bd0144b745bc33acd5ccd5ae3574
Andrew Tetteh <tettehandrew18@gmail.com>
2023-06-21 20:05:25 +0000

    Even Liber Abbaci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * description: prints even numbers of the fibonacci sequence
+ * less than 4000000
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	long int fib[34];
+	int a;
+	long int sum = 0;
+
+	fib[0] = 1;
+	fib[1] = 2;
+
+	for (a = 2; a < 35; a++)
+	{
+		fib[a] = fib[a - 1] + fib[a - 2];
+	}
+	for (a = 0; a < 35; a++)
+	{
+		if (fib[a] % 2 == 0)
+			sum += fib[a];
+	}
+	printf("%ld", sum);
+	return (0);
+}
