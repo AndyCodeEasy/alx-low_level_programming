f1d56d318b96676aa2d8bab34833595921b94a5d
AndyCodeEasy <tettehandrew18@gmail.com>
2023-08-04 02:13:13 +0000

    fixing errors

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -10,6 +10,7 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int uint;
+	int i, base_two;
 
 	if (!b)
 		return (0);
@@ -17,14 +18,17 @@ unsigned int binary_to_uint(const char *b)
 	uint = 0;
 
 	for (i = 0; b[i] != '\0'; i++)
+		;
+
+	for (i--, base_two = 1; i >= 0; i--, base_two *= 2)
 	{
-		if(b[i] != '0' || b[i] != '1')
-			return (0);
-		else
+		if (b[i] != '0' && b[i] != '1')
 		{
-			uint = uint << 1;
-			if (b[i] == '1')
-				uint = uint | 1;
+			return (0);
+		}
+		if (b[i] & 1)
+		{
+			uint += base_two;
 		}
 	}
 	return (uint);
